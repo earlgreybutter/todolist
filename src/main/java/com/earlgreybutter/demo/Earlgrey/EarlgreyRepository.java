50ed449c1351c1fb2270f5764d7ee208dedbd266
earlgreybutter <duckduck31216@gmail.com>
2020-07-22 19:27:25 +0900

    lettermod

--- src/main/java/com/earlgreybutter/demo/Earlgrey/EarlgreyRepository.java
+++ src/main/java/com/earlgreybutter/demo/Earlgrey/EarlgreyRepository.java
@@ -2,8 +2,10 @@ package com.earlgreybutter.demo.Earlgrey;
 
 import java.util.List;
 
+import org.apache.ibatis.annotations.Mapper;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+@Mapper
 public interface EarlgreyRepository extends JpaRepository<EarlgreyDto, String> {
     public List<EarlgreyDto> selectEarlgreyList() throws Exception;
 }
